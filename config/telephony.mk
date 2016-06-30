e7bf75f57be6a9e7e9773785d35acb4b47533925
angelcalibur <angelcalibur@gmail.com>
2016-06-30 12:18:57 +0200

    Update telephony.mk

--- config/telephony.mk
+++ config/telephony.mk
@@ -1,7 +1,7 @@
 # World APN list
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/jdc/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # Selective SPN list for operator number who has the problem.
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+    vendor/jdc/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
