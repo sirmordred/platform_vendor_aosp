de3f056b8ba2bc0087a2835a2b406f3af7c02b20
angelcalibur <angelcalibur@gmail.com>
2016-06-30 12:18:27 +0200

    Update common_full_phone.mk

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,4 +1,4 @@
-# Inherit common AOSP stuff
-$(call inherit-product, vendor/aosp/config/common.mk)
+# Inherit common JDC stuff
+$(call inherit-product, vendor/jdc/config/common.mk)
 
-$(call inherit-product, vendor/aosp/config/telephony.mk)
+$(call inherit-product, vendor/jdc/config/telephony.mk)
