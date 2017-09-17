c3d1d0f479f49f40332e2eb37a0eed590973d306
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-09-17 20:07:36 +0200

    (2/2) Fix soong flags
    This time for real
    
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,10 +1,10 @@
-# Insert new variables inside the JDC structure
+# Insert new variables inside the Jdc structure
 jdc_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
-	echo '"JDC": {'; \
+	echo '"Jdc": {'; \
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
-	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false),'; \
+	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
